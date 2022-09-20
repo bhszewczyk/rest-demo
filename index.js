c8a4226d22e0f74dbc8b192117a548aab3c39baa
Barbara Szewczyk <comamysza@gmail.com>
2022-09-20 14:43:21 +0200

    Add parsing the request payload

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// parse requests included in the body of the request
+app.use(express.urlencoded({ extended: true }));
+// parse json format payload
+app.use(express.json());
+
 app.get('/pizza', (req, res) => {
 	res.send('Here is the GET /pizza response');
 });
 
 app.post('/pizza', (req, res) => {
-	res.send('Here is the POST /pizza response');
+	const { meal, qty } = req.body;
+	res.send(
+		`Here is the POST /pizza response. You have ordered ${qty} ${meal}(s)`
+	);
 });
 
 app.listen(PORT, () => {
