4322ed62c5791c34c5602ce33453332ec2ea64ac
Barbara Szewczyk <comamysza@gmail.com>
2022-09-20 22:09:29 +0200

    Set up EJS

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require('path');
 
 // parse requests included in the body of the request
 app.use(express.urlencoded({ extended: true }));
 // parse json format payload
 app.use(express.json());
+// define absokute path to the views
+app.set('views', path.joine(__dirname, 'views'));
+// set up engine as EJS
+app.set('view engine', 'ejs');
 
 app.get('/pizza', (req, res) => {
 	res.send('Here is the GET /pizza response');
